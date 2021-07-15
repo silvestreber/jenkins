fbeef694d9890bae177b2cdca616aa6c20229daa
Juan <silvestrebermejo@gmail.com>
2021-07-15 16:10:12 +0200

    fix bugs

--- PruebaJenkins/src/main/java/com/example/demo/controller/Controlador.java
+++ PruebaJenkins/src/main/java/com/example/demo/controller/Controlador.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Controlador {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String prueba() {
-		return "WORKS";
+		return "<h1 align=\"center\">WORKS!<h1>";
 	}
 
 }
